1d8b4737e90c8be066d6b6908297e621c2342602
Connor Ingram <118000484+connor1005@users.noreply.github.com>
2023-03-09 13:53:06 -0500

    Update videotest.py

--- videotest.py
+++ videotest.py
@@ -1,1 +1,21 @@
+import pygame
+from sys import exit
 
+pygame.init()
+screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption('Game')
+clock = pygame.time.Clock()
+
+sky_surface = pygame.image.load(#sky.png)
+ground_surface = pygame.image.load(#ground.png)
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+   
+    screen.blit(ground_surface,(0,0))         
+    screen.blit(sky_surface,(0,300))
+    
+    pygame.display.update()
+    clock.tick(60)
